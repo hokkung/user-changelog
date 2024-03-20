cb7e30b9b5539f293231cc27b0b7b454ec420b11
Boonyarit Rousamran <hok@Boonyarits-MacBook-Air.local>
2024-03-19 22:12:22 -0700

    init

---
+++ src/main/java/com/leo/userchangelog/UserChangelogApplication.java
@@ -0,0 +1,13 @@
+package com.leo.userchangelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserChangelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserChangelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/userchangelog/UserChangelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.userchangelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserChangelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
